bce3b90759314e60afcf4a885d03339b7867ccef
University of Idaho <166424748+uiedu@users.noreply.github.com>
2024-05-26 21:56:11 -0700

    A Serial Communication VB software
    Open in Visual Studio (Not VS Code), and publish to create the exe file

---
+++ RS232/RS232.Designer.vb
@@ -0,0 +1,250 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmRS232
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbPort = New System.Windows.Forms.ComboBox()
+        Me.cmbBaud = New System.Windows.Forms.ComboBox()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.rtbReceived = New System.Windows.Forms.RichTextBox()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.txtFileFolder = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button_Send = New System.Windows.Forms.Button()
+        Me.cb_lineender = New System.Windows.Forms.ComboBox()
+        Me.Btn_AddForm = New System.Windows.Forms.Button()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 44)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Com Port:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 115)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(119, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Baud Rate:"
+        '
+        'cmbPort
+        '
+        Me.cmbPort.FormattingEnabled = True
+        Me.cmbPort.Location = New System.Drawing.Point(174, 38)
+        Me.cmbPort.Margin = New System.Windows.Forms.Padding(6)
+        Me.cmbPort.Name = "cmbPort"
+        Me.cmbPort.Size = New System.Drawing.Size(280, 33)
+        Me.cmbPort.TabIndex = 2
+        '
+        'cmbBaud
+        '
+        Me.cmbBaud.FormattingEnabled = True
+        Me.cmbBaud.Location = New System.Drawing.Point(174, 110)
+        Me.cmbBaud.Margin = New System.Windows.Forms.Padding(6)
+        Me.cmbBaud.Name = "cmbBaud"
+        Me.cmbBaud.Size = New System.Drawing.Size(280, 33)
+        Me.cmbBaud.TabIndex = 3
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(542, 34)
+        Me.btnConnect.Margin = New System.Windows.Forms.Padding(6)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(140, 44)
+        Me.btnConnect.TabIndex = 4
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Location = New System.Drawing.Point(542, 110)
+        Me.btnRefresh.Margin = New System.Windows.Forms.Padding(6)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(140, 44)
+        Me.btnRefresh.TabIndex = 5
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.rtbReceived)
+        Me.GroupBox2.Location = New System.Drawing.Point(24, 184)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(6)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(6)
+        Me.GroupBox2.Size = New System.Drawing.Size(1788, 798)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Received Data"
+        '
+        'rtbReceived
+        '
+        Me.rtbReceived.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rtbReceived.Location = New System.Drawing.Point(12, 46)
+        Me.rtbReceived.Margin = New System.Windows.Forms.Padding(6)
+        Me.rtbReceived.Name = "rtbReceived"
+        Me.rtbReceived.Size = New System.Drawing.Size(1704, 694)
+        Me.rtbReceived.TabIndex = 0
+        Me.rtbReceived.Text = ""
+        '
+        'SerialPort1
+        '
+        '
+        'txtFileFolder
+        '
+        Me.txtFileFolder.Location = New System.Drawing.Point(200, 998)
+        Me.txtFileFolder.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.txtFileFolder.Name = "txtFileFolder"
+        Me.txtFileFolder.Size = New System.Drawing.Size(480, 31)
+        Me.txtFileFolder.TabIndex = 14
+        Me.txtFileFolder.Text = "D:\Temp\Test.csv"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(32, 1006)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(6, 0, 6, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(73, 25)
+        Me.Label10.TabIndex = 13
+        Me.Label10.Text = "Folder"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(766, 992)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(112, 37)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(980, 990)
+        Me.btnClear.Margin = New System.Windows.Forms.Padding(6)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(110, 40)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(860, 51)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(592, 31)
+        Me.TextBox1.TabIndex = 15
+        '
+        'Button_Send
+        '
+        Me.Button_Send.Location = New System.Drawing.Point(1756, 44)
+        Me.Button_Send.Margin = New System.Windows.Forms.Padding(6)
+        Me.Button_Send.Name = "Button_Send"
+        Me.Button_Send.Size = New System.Drawing.Size(140, 44)
+        Me.Button_Send.TabIndex = 4
+        Me.Button_Send.Text = "Send"
+        Me.Button_Send.UseVisualStyleBackColor = True
+        '
+        'cb_lineender
+        '
+        Me.cb_lineender.FormattingEnabled = True
+        Me.cb_lineender.Location = New System.Drawing.Point(1527, 51)
+        Me.cb_lineender.Margin = New System.Windows.Forms.Padding(6)
+        Me.cb_lineender.Name = "cb_lineender"
+        Me.cb_lineender.Size = New System.Drawing.Size(158, 33)
+        Me.cb_lineender.TabIndex = 2
+        '
+        'Btn_AddForm
+        '
+        Me.Btn_AddForm.Location = New System.Drawing.Point(1688, 1006)
+        Me.Btn_AddForm.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.Btn_AddForm.Name = "Btn_AddForm"
+        Me.Btn_AddForm.Size = New System.Drawing.Size(208, 37)
+        Me.Btn_AddForm.TabIndex = 2
+        Me.Btn_AddForm.Text = "Add New Window"
+        Me.Btn_AddForm.UseVisualStyleBackColor = True
+        '
+        'frmRS232
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1928, 1062)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtFileFolder)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Btn_AddForm)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.Button_Send)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.cmbBaud)
+        Me.Controls.Add(Me.cb_lineender)
+        Me.Controls.Add(Me.cmbPort)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(6)
+        Me.MaximizeBox = False
+        Me.Name = "frmRS232"
+        Me.Text = "RS232 Communication"
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cmbPort As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbBaud As System.Windows.Forms.ComboBox
+    Friend WithEvents btnConnect As System.Windows.Forms.Button
+    Friend WithEvents btnRefresh As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents rtbReceived As System.Windows.Forms.RichTextBox
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents txtFileFolder As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button_Send As Button
+    Friend WithEvents cb_lineender As ComboBox
+    Friend WithEvents Btn_AddForm As Button
+End Class
---
+++ RS232/RS232.vb
@@ -0,0 +1,244 @@
+﻿'Serial Port Interfacing with VB.net 2010 Express Edition
+'Copyright (C) 2011  Dev Shrestha
+
+
+Imports System.ComponentModel
+
+'Imports Microsoft.VisualBasic.FileIO
+
+
+Public Class frmRS232
+
+    Dim myPort As Array  'COM Ports detected on the system will be stored here
+    Dim Sentence As String
+    Private _Connect As Boolean = True
+    Private _OpenForms As Integer = 0
+    Private _R2323s() As frmRS232
+    Delegate Sub SetTextCallback(ByVal [text] As String) 'Added to prevent threading errors during receiveing of data
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Port_Refresh()
+        cb_lineender.Items.Clear()
+        cb_lineender.Items.Add("None")
+        cb_lineender.Items.Add("Cr")
+        cb_lineender.Items.Add("Lf")
+        cb_lineender.Items.Add("CrLf")
+        cb_lineender.SelectedIndex = 0
+
+        Try
+
+            cb_lineender.Text = cmbPort.Items.Item(My.Settings.LineEnder)    'Set cmbPort text to the first COM port detected
+
+        Catch
+            cb_lineender.Text = "None"
+
+        End Try
+
+    End Sub
+
+    Private Sub btnConnect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnConnect.Click
+        If (_Connect) Then
+            My.Settings.Port = cmbPort.SelectedIndex
+            My.Settings.Baud = cmbBaud.SelectedIndex
+            My.Settings.Save()
+
+            SerialPort1.PortName = cmbPort.Text         'Set SerialPort1 to the selected COM port at startup
+            SerialPort1.BaudRate = cmbBaud.Text         'Set Baud rate to the selected value on 
+
+            'Other Serial Port Property
+            SerialPort1.Parity = IO.Ports.Parity.None
+            SerialPort1.StopBits = IO.Ports.StopBits.One
+            SerialPort1.DataBits = 8            'Open our serial port
+            SerialPort1.Open()
+            _Connect = False
+            btnConnect.Text = "Disconnect"
+            'btnConnect.Enabled = False          'Disable Connect button
+            'btnDisconnect.Enabled = True        'and Enable Disconnect button
+        Else
+            SerialPort1.Close()             'Close our Serial Port
+
+            'btnConnect.Enabled = True
+            'btnDisconnect.Enabled = False
+            _Connect = True
+            btnConnect.Text = "Connect"
+        End If
+
+    End Sub
+
+    Private Sub btnRefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRefresh.Click
+        Port_Refresh()
+
+    End Sub
+
+
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        ReceivedText(SerialPort1.ReadExisting())    'Automatically called every time a data is received at the serialPort
+    End Sub
+    Private Sub ReceivedText(ByVal [text] As String)
+        ' This function can be called multiple times per line
+        'compares the ID of the creating Thread to the ID of the calling Thread
+        'Dim TimeStamp As String
+        If Me.rtbReceived.InvokeRequired Then
+            Dim x As New SetTextCallback(AddressOf ReceivedText)
+            Me.Invoke(x, New Object() {(text)})
+        Else
+            'TimeStamp = Format(Now(), "mm/dd/yyyy hh:mm:ss") + vbTab
+            'Sentence &= [text]
+            rtbReceived.Text &= [text]
+            rtbReceived.SelectionStart = rtbReceived.Text.Length
+            rtbReceived.ScrollToCaret()
+            'Fields = Sentence.Split(vbCrLf)
+
+            'If UBound(Fields) > 0 Then ' if there is more than one sentences
+            'Parse(Fields(0))
+            'Sentence = LTrim(Fields(UBound(Fields)))
+
+            'End If
+
+        End If
+    End Sub
+
+    Private Sub cmbPort_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbPort.SelectedIndexChanged
+        If SerialPort1.IsOpen = False Then
+            SerialPort1.PortName = cmbPort.Text         'pop a message box to user if he is changing ports
+        Else                                            'without disconnecting first.
+            MsgBox("Valid only if port is Closed", vbCritical)
+        End If
+    End Sub
+
+    Private Sub cmbBaud_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbBaud.SelectedIndexChanged
+        If SerialPort1.IsOpen = False Then
+            SerialPort1.BaudRate = cmbBaud.Text         'pop a message box to user if he is changing baud rate
+        Else                                            'without disconnecting first.
+            MsgBox("Valid only if port is Closed", vbCritical)
+        End If
+    End Sub
+    ' This function attempts to open the passed Comm Port. If it is
+    '   available, it returns True, else it returns False. To determine
+    '   availability a Try-Catch block is used.
+    Private Function IsPortAvailable(ByVal ComPort As Integer) As Boolean
+
+        Try
+            SerialPort1.PortName = myPort(ComPort)         'Set SerialPort1 to the selected COM port at startup
+            SerialPort1.BaudRate = 9600         'Set Baud rate to the selected value on 
+
+            'Other Serial Port Property
+            SerialPort1.Parity = IO.Ports.Parity.None
+            SerialPort1.StopBits = IO.Ports.StopBits.One
+            SerialPort1.DataBits = 8            'Open our serial port
+            SerialPort1.Open()
+
+            ' If it makes it to here, then the Comm Port is available.
+            SerialPort1.Close()
+            Return True
+        Catch
+            ' If it gets here, then the attempt to open the Comm Port
+            '   was unsuccessful.
+            Return False
+        End Try
+    End Function
+
+
+
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        Dim File As String = txtFileFolder.Text
+        If Dir(File) <> "" Then
+            If (MsgBox("File exists." + vbCrLf + "Replace?", vbYesNo + vbCritical + 256, "Confirm") = vbYes) Then
+                Dim out As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(File, False)
+                out.Write(rtbReceived.Text)
+                out.Close()
+            End If
+        Else
+            Dim out As IO.StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(File, False)
+            out.Write(rtbReceived.Text)
+            out.Close()
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        Sentence = ""
+        rtbReceived.Text = ""
+    End Sub
+
+
+
+    Sub Port_Refresh()
+        'When our form loads, auto detect all serial ports in the system and populate the cmbPort Combo box.
+
+        cmbBaud.Items.Clear()
+        cmbPort.Items.Clear()
+        myPort = IO.Ports.SerialPort.GetPortNames() 'Get all com ports available
+
+        cmbBaud.Items.Add(9600)     'Populate the cmbBaud Combo box to common baud rates used   
+        cmbBaud.Items.Add(19200)
+        cmbBaud.Items.Add(38400)
+        cmbBaud.Items.Add(57600) 'For Wintec
+        cmbBaud.Items.Add(115200)
+        cmbBaud.Items.Add(230400)
+
+        For i = 0 To UBound(myPort)
+            If IsPortAvailable(i) Then cmbPort.Items.Add(myPort(i))
+        Next
+
+        Try
+
+            cmbPort.Text = cmbPort.Items.Item(My.Settings.Port)    'Set cmbPort text to the first COM port detected
+            cmbBaud.Text = cmbBaud.Items.Item(My.Settings.Baud)    'Set cmbBaud text to the first Baud rate on the list
+        Catch
+            If (cmbPort.Items.Count > 0) Then
+                cmbPort.Text = cmbPort.Items(cmbPort.Items.Count - 1)
+                cmbBaud.Text = 9600
+            Else
+                cmbPort.Text = "No avaialbe port"
+                cmbBaud.Text = 9600
+            End If
+
+        End Try
+
+
+        'btnRefresh.Enabled = False           'Initially Disconnect Button is Disabled
+    End Sub
+
+    Private Sub btnSend_Click(sender As Object, e As EventArgs) Handles Button_Send.Click
+        Dim TxtOut = TextBox1.Text
+        If (cb_lineender.SelectedIndex = 1) Then
+            TxtOut += vbCr
+        ElseIf (cb_lineender.SelectedIndex = 1) Then
+            TxtOut += vbLf
+        ElseIf (cb_lineender.SelectedIndex = 2) Then
+            TxtOut += vbCrLf
+        End If
+        If SerialPort1.IsOpen Then
+            SerialPort1.Write(TxtOut)
+        Else
+            MsgBox("Serial port not open", vbCritical)
+        End If
+
+    End Sub
+
+    Private Sub Btn_AddForm_Click(sender As Object, e As EventArgs) Handles Btn_AddForm.Click
+        _OpenForms += 1
+        ReDim _R2323s(_OpenForms)
+        _R2323s(_OpenForms) = New frmRS232
+        _R2323s(_OpenForms).Show()
+    End Sub
+
+    Private Sub frmRS232_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        If (Me._Connect = False) Then
+            Me.SerialPort1.Close()
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox1.KeyPress
+
+        If (e.KeyChar = vbCr) Then
+            Me.Button_Send.PerformClick()
+        End If
+    End Sub
+
+
+End Class
